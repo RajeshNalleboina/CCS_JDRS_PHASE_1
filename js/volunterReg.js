6070915f718b06105c539623442f188830798d60
unknown <rajeshnalleboina94@gmail.com>
2022-12-03 08:27:21 +0530

    phase1 first commit

---
+++ js/volunterReg.js
@@ -0,0 +1,19 @@
+// let volunterName= document.querySelector('#vreg-name');
+// let volunterEmail= document.querySelector('#vreg-email');
+// let volunterPhone= document.querySelector('#vreg-mob');
+// let volunterAddress= document.querySelector('#vreg-textarea');
+// let volunterPassword= document.querySelector('#vreg-textarea');
+// document.querySelector('.vreg-submit').addEventListener("click", vregSub);
+// function vregSub() {
+//   if(volunterName!='' && volunterEmail!='' && volunterPhone!='' && volunterAddress!='') {
+//     let volunterData = {
+//       name:volunterName,
+//       email:volunterEmail,
+//       phone:volunterPhone,
+//       address:volunterAddress,
+//     }
+//     alert('details saved successfully!')
+//   } else {
+//     alert("please fill all details");
+//   }
+// }
